1144d6d59a87cba1b288d4ef8818accae9517789
julifors <julifors@outlook.com>
2023-12-27 16:58:37 +0100

    Initial setup of HTML and JS files
    Setup of HTML document with basic elements, tags, styling and some pseudocode placeholders
    
    Imported fonts from Google and linked to external JS file
    
    I also added pseudocode in JS file

---
+++ juliaforslund.js
@@ -0,0 +1,40 @@
+// get all required document elements for script and save in variables
+
+
+// Validation for fname and lname fields - required + contain only letters
+    // use regular expression to match input to valid format
+    // if not valid or empty
+        // show error message text - written in document under the incorrect field
+        // do not submit
+    // else return input
+
+
+// Validation for email field - valid format 
+    // use regular expression to match input to valid format
+    // if not valid or empty
+        // show error message text - written in document under the incorrect field
+        // do not submit
+    // else return input
+
+
+// Validation required questions not left unanswered
+    // validation to be done when submit button is clicked
+    // show error message text if validation error - written in document under the incorrect field
+        // if errors corrected hide error message
+        // do not submit
+    // if no errors show success message
+
+
+// Calculating total score
+    // variable count
+    // label correct answers (radio, checkbox)
+    // for open-ended question: check if correct answers in input
+
+// Show result of quiz
+    // no. correct answers out of total no. of answers
+    // display in displayResult div
+
+// Correct answers display
+    // show correct answers on the page
+    // show correct answers at the end of the quiz
+        // mark or write correct answer in green on/by/under the question
